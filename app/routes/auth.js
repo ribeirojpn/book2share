a37efea007fc58cbfda41eb2d91e8e5a90acc536
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2015-09-02 16:37:33 -0300

    oauth google and facebook

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -7,6 +7,20 @@ module.exports = function(app) {
     failureRedirect: '/#/login'
   }));
 
+  app.get('/auth/facebook', passport.authenticate('facebook',{scope: 'email'}));
+  app.get('/auth/facebook/callback',passport.authenticate('facebook',{
+    successRedirect: '/',
+    failureRedirect: '/#/login'
+  }));
+
+  app.get('/auth/google', passport.authenticate('google',{
+    scope: ['https://www.googleapis.com/auth/plus.login','email']
+  }));
+  app.get('/auth/google/callback',passport.authenticate('google',{
+    successRedirect: '/',
+    failureRedirect: '/#/login'
+  }));
+
   app.get('/logout', function (req,res) {
     req.logOut();
     res.redirect('/');
--- config/passport.js
+++ config/passport.js
@@ -29,6 +29,47 @@ module.exports = function () {
     )
   }));
 
+  passport.use(new FacebookStrategy({
+    clientID: '1026586744032934',
+    clientSecret: '51c5dc852108a92425c1f1983d7d5d5b',
+    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback",
+    profileFields: ['id','displayName','photos','emails']
+  }, function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate(
+      {'login':profile.emails[0].value},
+      {'name':profile.displayName,
+       'photo': profile.photos[0].value},
+      function (erro,user) {
+        if(erro){
+          console.log(erro);
+          return done(erro);
+        }
+        return done(null,user);
+
+       }
+    )
+  }));
+
+  passport.use(new GoogleStrategy({
+    clientID: '192440724904-4ea01dmm26kglbsa6p6njm4mbmrlrppq.apps.googleusercontent.com',
+    clientSecret: '1z0XYhqoIABVRp7zzYZw-Cz-',
+    callbackURL: "http://127.0.0.1:3000/auth/google/callback",
+    profileFields: ['id','displayName','photos','emails']
+  },function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(
+      {'login': profile.emails[0].value},
+      {'name': profile.displayName,
+      'photo': profile.photos[0].value},
+      function (erro, user) {
+        if(erro){
+          console.log(erro);
+          return done(erro);
+        }
+        return done(null,user);
+      }
+    )
+  }));
+
   passport.serializeUser(function (user,done) {
     done(null,user._id);
   });
